def05bf2c3f6646179c7699dcd5f867a7ed79548
Jeremy Maslanko <maslankoj@gmail.com>
2023-09-21 20:29:24 -0400

    update sf12 scoring

--- SF12/scoring.py
+++ SF12/scoring.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 raw_mapping = {
     'PF': ['Q2A', 'Q2B'],
@@ -12,46 +12,145 @@ raw_mapping = {
     'MH': ['Q6A', 'Q6C']
 }
 
+low_high_mapping = {
+    'PF': {'MAX': 6, 'MIN': 2, 'RANGE': 4},
+    'RP': {'MAX': 10, 'MIN': 2, 'RANGE': 8},
+    'BP': {'MAX': 5, 'MIN': 1, 'RANGE': 4},
+    'GH': {'MAX': 5, 'MIN': 1, 'RANGE': 4},
+    'VT': {'MAX': 5, 'MIN': 1, 'RANGE': 4},
+    'SF': {'MAX': 5, 'MIN': 1, 'RANGE': 4},
+    'RE': {'MAX': 10, 'MIN': 2, 'RANGE': 8},
+    'MH': {'MAX': 10, 'MIN': 2, 'RANGE': 8}
+}
 
-def raw(raw_data: pd.DataFrame):
+std_mapping = {
+    'PF': {'Mean': 81.18122, 'SD': 29.10558},
+    'RP': {'Mean': 80.52856, 'SD': 27.13526},
+    'BP': {'Mean': 81.74015, 'SD': 24.53019},
+    'GH': {'Mean': 72.19795, 'SD': 23.19041},
+    'VT': {'Mean': 55.59090, 'SD': 24.84380},
+    'SF': {'Mean': 83.73973, 'SD': 24.75775},
+    'RE': {'Mean': 86.41051, 'SD': 22.35543},
+    'MH': {'Mean': 70.18217, 'SD': 20.50597}
+}
+
+def q1_mapping(value):
+
+    question1_codes = {
+        1:5.0
+        ,2:4.4
+        ,3:3.4
+        ,4:2.0
+        ,5:1.0
+    }
+
+    return question1_codes[value]
+
+def rev_mapping(value):
+
+    reverse_codes = {
+        1:5
+        ,2:4
+        ,3:3
+        ,4:2
+        ,5:1
+        }
+
+    return reverse_codes[value]
+
+
+def recalibrate(raw_data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Recalibrate and reverse score the questions.  
+    Additionally, copies of the original columns will be made.
+
+    input: raw_data as a pandas df
+    output: processed data as a pandas df
+    '''
+    data = raw_data
+    cols = ['Q1', 'Q2A', 'Q2B', 'Q3A', 'Q3B', 'Q4A', 'Q4B', 'Q5', 'Q6A', 'Q6B', 'Q6C', 'Q7']
+    for column in cols:
+        data['original' + column] = data[column]
+
+    data['Q5'] = data['Q5'].apply(rev_mapping)
+    data['Q6A'] = data['Q6A'].apply(rev_mapping)
+    data['Q6B'] = data['Q6B'].apply(rev_mapping)
+    data['Q1'] = data['Q1'].apply(q1_mapping)
+
+    return data
+
+
+def raw(raw_data: pd.DataFrame) -> pd.DataFrame:
 
     data = raw_data
 
     for value in raw_mapping:
-        data['raw' + str(value)] = 0
+        data['Raw_' + str(value)] = 0
         for column in raw_mapping[value]:
-            data['raw' + str(value)] = data['raw' + str(value)] + data[column]
+            data['Raw_' + str(value)] = data['Raw_' + str(value)] + data[column]
     
     return data
 
 
-def transformed():
-    return
+def transformed(df: pd.DataFrame) -> pd.DataFrame:
+    
+    for value in low_high_mapping:
+        df['Transformed_'+value] = ((df['Raw_' + str(value)] - low_high_mapping[value]['MIN'])/low_high_mapping[value]['RANGE'])*100
+    
+    return df
 
 
-def standardized():
-    return
+def standardized(df: pd.DataFrame) -> pd.DataFrame:
+
+    for value in std_mapping:
+        df['Standardized_'+value] = (df['Transformed_'+str(value)] - std_mapping[value]['Mean'])/ std_mapping[value]['SD']
+    
+    return df
 
 
-def norm_based():
-    return
+def norm_based(df: pd.DataFrame) -> pd.DataFrame:
+    
+    for value in std_mapping:
+        df['NormBased_'+str(value)] = (df['Standardized_'+value]*10)+50
+    
+    return df
 
 
-def norm_based():
-    return
+def agg_pcs(df: pd.DataFrame) -> pd.DataFrame:
+
+    cols = [col for col in df.columns if 'Standardized' in col]
+    sub_df = df[cols].to_numpy()
+    values = np.array([.42402,.35119, .31754, .24954, .02877, -.00753, -.19206, -.22069])
+    
+    df['AGG_PCS'] = np.dot(sub_df, values)
+    
+    
+    return df
 
 
-def norm_based():
-    return
+def agg_mcs(df: pd.DataFrame) -> pd.DataFrame:
 
+    cols = [col for col in df.columns if 'Standardized' in col]
+    sub_df = df[cols].to_numpy()
+    values = np.array([-.22999, -.12329, -.09731, -.01571, .23534, .26876, .43407, .48581])
+    
+    df['AGG_MCS'] = np.dot(sub_df, values)
+    return df
 
-def agg():
-    return
+def pcs_mcs(df: pd.DataFrame) -> pd.DataFrame:
 
+    df['PCS'] = (df['AGG_PCS']*10)+50
+    df['MCS'] = (df['AGG_MCS']*10)+50
 
-def pcs_mcs():
-    return
-
+    return df
 
 if __name__ == "__main__":
     raw_data = pd.read_csv('data/SF12_A.csv')
+    data1 = recalibrate(raw_data)
+    data2 = raw(data1)
+    data3 = transformed(data2)
+    data4 = standardized(data3)
+    data5 = norm_based(data4)
+    data6 = agg_pcs(data5)
+    data7 = agg_mcs(data6)
+    final_data = pcs_mcs(data7)
