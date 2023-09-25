a1b3614724d2343e5f60325bf643d1e99e49303c
Jeremy Maslanko <maslankoj@gmail.com>
2023-09-25 19:20:32 -0400

    finalize sf12A scoring

--- SF12/scoringSF12.py
+++ SF12/scoringSF12.py
@@ -34,6 +34,8 @@ std_mapping = {
     'MH': {'Mean': 70.18217, 'SD': 20.50597}
 }
 
+COLUMNS = ['Q1', 'Q2A', 'Q2B', 'Q3A', 'Q3B', 'Q4A', 'Q4B', 'Q5', 'Q6A', 'Q6B', 'Q6C', 'Q7']
+
 def q1_mapping(value):
 
     question1_codes = {
@@ -59,7 +61,7 @@ def rev_mapping(value):
     return reverse_codes[value]
 
 
-def recalibrate(raw_data: pd.DataFrame) -> pd.DataFrame:
+def recalibrate(input_df: pd.DataFrame) -> pd.DataFrame:
     '''
     Recalibrate and reverse score the questions.  
     Additionally, copies of the original columns will be made.
@@ -67,10 +69,10 @@ def recalibrate(raw_data: pd.DataFrame) -> pd.DataFrame:
     input: raw_data as a pandas df
     output: processed data as a pandas df
     '''
-    data = raw_data
-    cols = ['Q1', 'Q2A', 'Q2B', 'Q3A', 'Q3B', 'Q4A', 'Q4B', 'Q5', 'Q6A', 'Q6B', 'Q6C', 'Q7']
-    for column in cols:
-        data['original' + column] = data[column]
+    data = input_df.copy()
+    # cols = ['Q1', 'Q2A', 'Q2B', 'Q3A', 'Q3B', 'Q4A', 'Q4B', 'Q5', 'Q6A', 'Q6B', 'Q6C', 'Q7']
+    for column in COLUMNS:
+        data['orginal' + column] = data[column]
 
     data['Q5'] = data['Q5'].apply(rev_mapping)
     data['Q6A'] = data['Q6A'].apply(rev_mapping)
@@ -80,9 +82,9 @@ def recalibrate(raw_data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
-def raw(raw_data: pd.DataFrame) -> pd.DataFrame:
+def raw(input_df: pd.DataFrame) -> pd.DataFrame:
 
-    data = raw_data
+    data = input_df.copy()
 
     for value in raw_mapping:
         data['Raw_' + str(value)] = 0
@@ -92,31 +94,37 @@ def raw(raw_data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
-def transformed(df: pd.DataFrame) -> pd.DataFrame:
+def transformed(data: pd.DataFrame) -> pd.DataFrame:
     
+    df = data.copy()
+
     for value in low_high_mapping:
         df['Transformed_'+value] = ((df['Raw_' + str(value)] - low_high_mapping[value]['MIN'])/low_high_mapping[value]['RANGE'])*100
     
     return df
 
 
-def standardized(df: pd.DataFrame) -> pd.DataFrame:
+def standardized(data: pd.DataFrame) -> pd.DataFrame:
 
+    df = data.copy()
     for value in std_mapping:
         df['Standardized_'+value] = (df['Transformed_'+str(value)] - std_mapping[value]['Mean'])/ std_mapping[value]['SD']
     
     return df
 
 
-def norm_based(df: pd.DataFrame) -> pd.DataFrame:
+def norm_based(data: pd.DataFrame) -> pd.DataFrame:
     
+    df = data.copy()
     for value in std_mapping:
         df['NormBased_'+str(value)] = (df['Standardized_'+value]*10)+50
     
     return df
 
 
-def agg_pcs(df: pd.DataFrame) -> pd.DataFrame:
+def agg_pcs(data: pd.DataFrame) -> pd.DataFrame:
+
+    df = data.copy()
 
     cols = [col for col in df.columns if 'Standardized' in col]
     sub_df = df[cols].to_numpy()
@@ -128,7 +136,9 @@ def agg_pcs(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def agg_mcs(df: pd.DataFrame) -> pd.DataFrame:
+def agg_mcs(data: pd.DataFrame) -> pd.DataFrame:
+
+    df = data.copy()
 
     cols = [col for col in df.columns if 'Standardized' in col]
     sub_df = df[cols].to_numpy()
@@ -137,20 +147,27 @@ def agg_mcs(df: pd.DataFrame) -> pd.DataFrame:
     df['AGG_MCS'] = np.dot(sub_df, values)
     return df
 
-def pcs_mcs(df: pd.DataFrame) -> pd.DataFrame:
+def pcs_mcs(data: pd.DataFrame) -> pd.DataFrame:
 
+    df = data.copy()
     df['PCS'] = (df['AGG_PCS']*10)+50
     df['MCS'] = (df['AGG_MCS']*10)+50
 
     return df
 
+
+
 if __name__ == "__main__":
     raw_data = pd.read_csv('data/SF12_A.csv')
-    data1 = recalibrate(raw_data)
-    data2 = raw(data1)
-    data3 = transformed(data2)
-    data4 = standardized(data3)
-    data5 = norm_based(data4)
-    data6 = agg_pcs(data5)
-    data7 = agg_mcs(data6)
-    final_data = pcs_mcs(data7)
+    data1 = recalibrate(raw_data).copy()
+    data2 = raw(data1).copy()
+    data3 = transformed(data2).copy()
+    data4 = standardized(data3).copy()
+    data5 = norm_based(data4).copy()
+    data6 = agg_pcs(data5).copy()
+    data7 = agg_mcs(data6).copy()
+    final_data = pcs_mcs(data7).copy()
+    final_data = final_data.drop(columns=COLUMNS).copy()
+    final_data.columns = [col.replace('orginal', '') for col in final_data.columns]
+    final_data = final_data.round(2).copy()
+    final_data.to_csv('data/SF12_A_OUTPUT.csv')
